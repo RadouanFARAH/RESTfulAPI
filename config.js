daf7eaea48cb6948bf08507d484a8ebb7b768282
Redouane FARAH <50687682+FARAH-Redouane@users.noreply.github.com>
2019-12-28 14:14:51 +0000

    passport-facebook

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        ClientId : '1004546349904867',
+        ClientSecret : '79d5a956f60d9a976d0286923e5ae103'
+    }
 }
